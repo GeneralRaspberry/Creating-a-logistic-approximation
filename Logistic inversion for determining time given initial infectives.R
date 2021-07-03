f9ca11dee6c08bd9795a1f9374a516d5fc17e9d3
Thomas Taylor <t.h.taylor@outlook.com>
2021-07-03 12:52:38 +0100

    Add files via upload

--- Logistic inversion for determining time given initial infectives.R
+++ Logistic inversion for determining time given initial infectives.R
@@ -12,3 +12,35 @@ infbegin<-infbegin[1:997]#eliminating bad data points
 ######################################plot#############################################################
 timeinfbeginplot<-plot(t,infbegin)
 timeinfbeginplot
+
+
+############################translating the optimisation function to my plot###########################
+
+
+theta<-1
+beta<-10
+maxd<-100
+eq<-function(x){
+  discale<-exp(-x/theta)
+  k.norm <- beta * (1/(2*pi*theta^2))
+  f<-k.norm*discale
+  return(f)}
+ggplot(data.frame(x=c(0, maxd)), aes(x=x)) + 
+  stat_function(fun=eq)+theme_minimal()+#theme(axis.text.y = element_blank())+
+  ylab("Infection Likelihood")+
+  xlab("Distance from Source")
+
+intpd <- integrate(eq, 0, maxd)
+intpdm <- intpd$value/maxd - 0
+
+opt<-optim(50, fn = function(x) (eq(x) - intpdm)^2, 
+           method = 'Brent', lower = 0, upper = maxd)$par
+
+pdplot1<-ggplot(data.frame(x=c(0, maxd)), aes(x=x)) + 
+  stat_function(fun=eq) +
+  theme_minimal() +
+  ylab("Infection Likelihood") +
+  xlab("Distance from Source") +
+  geom_hline(yintercept = intpdm) + 
+  geom_vline(xintercept = opt) +
+  ggtitle(expression(paste(theta," = 1")))
